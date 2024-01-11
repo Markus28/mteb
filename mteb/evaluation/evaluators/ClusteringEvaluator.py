42dc279a64899e4796ff70c18d76cfb7de0cc9d6
Markus Krimmel <markus.krimmel@jina.ai>
2024-01-11 12:46:44 +0100

    feat: added spherical-k-means

--- mteb/evaluation/evaluators/ClusteringEvaluator.py
+++ mteb/evaluation/evaluators/ClusteringEvaluator.py
@@ -31,6 +31,10 @@ class ClusteringEvaluator(Evaluator):
             clustering_model = sklearn.cluster.MiniBatchKMeans(
                 n_clusters=K, batch_size=self.clustering_batch_size, n_init="auto"
             )
+        elif self.clustering_algorithm == 'spherical-k-means':
+            clustering_model = sklearn.cluster.MiniBatchKMeans(
+                n_clusters=K, batch_size=self.clustering_batch_size, n_init="auto", spherical=True,
+            )
         elif self.clustering_algorithm == 'agglomerative':
             clustering_model = sklearn.cluster.AgglomerativeClustering(n_clusters=K, affinity='cosine', linkage='average')
         else:
